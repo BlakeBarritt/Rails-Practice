386e44ce0d0dae4ef9a5f3a55ebd9f869dca8a7a
Blake Barritt <bbarritt1@gmail.com>
2022-06-30 11:41:45 -0700

    add conditional rendering of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,9 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     # @ converts to instance variable
   end
+
+  def index
+    @articles = Article.all
+      
+  end
 end
