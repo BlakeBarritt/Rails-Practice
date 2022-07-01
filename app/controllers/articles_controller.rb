92a0433e251a95dee02188aa43d839337cb4f59f
Blake Barritt <bbarritt1@gmail.com>
2022-06-30 20:27:57 -0700

    read/update add

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,37 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-      
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article
+    if @article.save
+      flash[:notice] = "New Article saved"
+      redirect_to @article
+    else
+      render 'new'
+      # instance variable available in 'new' template
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+    # update does not require save
+      flash[:notice] = 'Article updated successfully'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 end
